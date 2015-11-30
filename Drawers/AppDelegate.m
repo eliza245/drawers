d82d47e5221e26d970e2b23ee6cef60255d3cb50
Eliza Herring <ehh245@nyu.edu>
2015-11-30 13:49:39 -0500

    parse app delegate

--- Drawers/AppDelegate.m
+++ Drawers/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -16,6 +17,17 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // [Optional] Power your app with Local Datastore. For more info, go to
+    // https://parse.com/docs/ios_guide#localdatastore/iOS
+    [Parse enableLocalDatastore];
+    
+    // Initialize Parse.
+    [Parse setApplicationId:@"A1YXKGALWR6VYhy9kpI5OKZJLO40MGXInlbyZMIc"
+                  clientKey:@"SmVFZ1I1naKH9CdaWmqRC6xZKJsb7BXLZfCbuqc0"];
+    
+    // [Optional] Track statistics around application opens.
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+    
     // Override point for customization after application launch.
     return YES;
 }
